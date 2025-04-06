1b54f36e88a3ac17e75b7df08203f658bb619ff7
Juan Brito <juanbritograngeiro@gmail.com>
2025-04-06 18:37:30 -0300

    Criação de Calculadora de número Fatorial

---
+++ logica-js-projeto_inicial/numeroFatorial/app.js
@@ -0,0 +1,29 @@
+function numeroFatorial(n) {
+    if (n < 0) return undefined;
+    if (n === 0 || n === 1 ) return 1;
+    return n *numeroFatorial(n-1);
+}
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function limparCampo() {
+    campo = document.querySelector('input');
+    campo.value = '';
+}
+
+function calcular() {
+    let n = parseInt(document.querySelector('input').value);
+    let resultado = numeroFatorial(n);
+    exibirTextoNaTela('h1', `O resultado é: ${resultado}`);
+    limparCampo();
+    document.getElementById('reiniciar').removeAttribute('disabled');
+}
+
+function calcularOutroNum() {
+
+}
+
+
